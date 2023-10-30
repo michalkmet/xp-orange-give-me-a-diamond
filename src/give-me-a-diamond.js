9779b0cbe97126c6a3f0d31d6e6ecda34851ccbe
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-10-30 12:25:32 +0100

    UAT2.3 in progress

--- src/give-me-a-diamond.js
+++ src/give-me-a-diamond.js
@@ -7,22 +7,41 @@ function diamond(inputNumber) {
 }
 
 function createDiamond(inputNumber) {
-  let result;
+  let result = '';
   if (inputNumber === 1) {
     result = '*';
   } else {
-    for (let i = 0; i < inputNumber; i++) {
+    let levels = (inputNumber - 1) / 2;
+    console.log('levels: ', levels);
+    for (let i = 1; i <= inputNumber; i++) {
       console.log('i: ', i);
+      if (i === 1) { // first line
+        for (let j = 1; j <= levels; j++) {
+          result += ' ';
+        }
+        result += '*';
+        result += '\n';
+      } else if (i === inputNumber) { // last line
+        for (let k = 1; k <= levels; k++) {
+          result += ' ';
+        }
+        result += '*';
+        result += '\n';
+      } else { // between the first and last
+        
+        if (i % 2 != 0) {
+          
+        }
+        
+        for (let l = 1; l <= inputNumber; l++) {
+          result += '*';
+        }
+        result += '\n';
+      }
+
     }
-    result += ' ';
-    result += '*';
-    result += '\n';
-    result += '***';
-    result += '\n';
-    result += ' ';
-    result += '*';
-    result += '\n';
   }
+  console.log('result: ', result);
   return result;
 }
 module.exports = diamond;
